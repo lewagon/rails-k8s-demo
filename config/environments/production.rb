1792c1c40e14b6c5b15d086fd9669243fc084858
Andy B <andybarnov@gmail.com>
2019-11-08 16:42:20 +0100

    use sidekiq as activejob adaptor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
+  config.active_job.queue_adapter     = :sidekiq
   # config.active_job.queue_name_prefix = "review_scraper_production"
 
   config.action_mailer.perform_caching = false
