6cc5fab37a1e90fc018b1ded915beb642d9ee044
Andy B <andybarnov@gmail.com>
2019-11-12 14:11:02 +0100

    rename app

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development do
   gem "spring"
   gem "spring-watcher-listen", "~> 2.0.0"
   gem "httplog"
+  gem "rename"
 end
 
 group :test do
--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ReviewScraper
+module RailsK8sDemo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
