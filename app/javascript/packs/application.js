d86d57c57d902a559067ecce9a752ff2a12f554e
Andy B <andybarnov@gmail.com>
2019-12-13 17:58:45 +0100

    get rid of a height clutch, need to find new way to optimize for Safari

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,3 @@ require("turbolinks").start();
 
 import "controllers";
 import "../stylesheets/application";
-
-// get the actual viewport height on mobile browsers
-window.addEventListener("DOMContentLoaded", event => {
-  const chat = document.querySelector("#chat");
-  const messagesContainer = document.querySelector("#messages-container");
-  const viewPortH = chat.getBoundingClientRect().height;
-  const windowH = window.innerHeight;
-  const browserUiBarsH = viewPortH - windowH;
-  chat.style.height = `calc(100vh - ${browserUiBarsH}px)`;
-  messagesContainer.style.height = `calc(70vh - ${browserUiBarsH}px)`;
-});
